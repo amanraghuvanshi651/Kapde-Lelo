b01871d1235520de6ef6f54d5a35d48fd85a492d
macmini50 <macmini50@macmini50s-Mac-mini.local>
2024-02-09 19:39:00 +0530

    Created Kapde Lelo

---
+++ Kapde Lelo/Kapde_LeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kapde_LeloApp.swift
+//  Kapde Lelo
+//
+//  Created by macmini50 on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Kapde_LeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
